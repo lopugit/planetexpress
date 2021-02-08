709539a92f3dd975b461018724101b9eb8457b27
nikolaj <lopudesigns@gmail.com>
2021-02-08 16:49:30 +1100

    added serve config to pm2 ecosystem and built planetexpress

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,11 +10,20 @@ module.exports = {
     max_memory_restart: '1G',
     env: {
 			api: 'dev',
-      NODE_ENV: 'development'
     },
-    env_production: {
-      NODE_ENV: 'production'
-    }
+  },
+  {
+    name: 'planetexpress',
+		script: 'quasar',
+    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    args: 'serve --api=prod',
+    instances: 2,
+    autorestart: true,
+    watch: true,
+    max_memory_restart: '1G',
+    env: {
+			api: 'prod',
+    },
   }]
 };
 
