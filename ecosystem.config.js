3c118482a79bd38dc798c062a9626c65baf02f77
Lopu <lopu@ip-172-31-34-179.ap-southeast-2.compute.internal>
2021-08-04 13:01:43 +0000

    dev

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,6 @@ module.exports = {
 		script: 'quasar',
     // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
     args: 'dev --api=dev',
-    instances: 1,
     autorestart: true,
     watch: true,
     max_memory_restart: '1G',
